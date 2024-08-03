b19fd7c12eb05c88a255faf27c30939b03fbafe2
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-08-04 03:55:30 +0530

    buy and create token

--- contracts/src/MintMania.sol
+++ contracts/src/MintMania.sol
@@ -4,18 +4,58 @@ pragma solidity ^0.8.13;
 import "./Token.sol";
 import "@openzeppelin-contracts-5.0.2/access/Ownable.sol";
 
+struct TokenInfo {
+    string name;
+    string symbol;
+    string uri;
+}
+
 contract MintMania is Ownable {
+    uint256 public constant MAX_SUPPLY = 1000_000_000; // 1 billion
+    uint256 public constant INITIAL_SUPPLY = 100_000_000; // 1 million (10% of max supply)
+
+    mapping(address => TokenInfo) private tokens;
+    mapping(address => mapping(address => uint256)) private balances;
+    mapping(address => uint256) private tokenEthBalance;
+
     constructor(address owner) Ownable(owner) {}
 
     event TokenCreated(address token, string name, string symbol);
+    event TokenBought(address token, address buyer, uint256 amount);
 
-    function createToken(string memory name, string memory symbol) public onlyOwner {
-        // Create a new token with the specified name and symbol
-        Token token = new Token(name, symbol);
+    function create(
+        string memory name,
+        string memory symbol,
+        string memory uri
+    ) public {
+        require(bytes(name).length > 0, "Name cannot be empty");
+        require(bytes(symbol).length > 0, "Symbol cannot be empty");
+        require(bytes(uri).length > 0, "URI cannot be empty");
 
-        token.
-        
+        Token token = new Token(name, symbol, INITIAL_SUPPLY, MAX_SUPPLY);
+        tokens[address(token)] = TokenInfo(name, symbol, uri);
         emit TokenCreated(address(token), name, symbol);
     }
 
+    // amount in token
+    function buy(address token, uint256 amount) public payable {
+        require(bytes(tokens[token].name).length > 0, "Token does not exist");
+        require(amount > 0, "Amount must be greater than 0");
+
+        uint256 amountEth = getPricePerToken(token) * amount;
+        require(msg.value >= amountEth, "Insufficient funds sent");
+
+        balances[token][msg.sender] += amount;
+        tokenEthBalance[token] += amountEth;
+
+        emit TokenBought(token, msg.sender, amount);
+    }
+
+    function getPricePerToken(address token) public view returns (uint256) {
+        require(bytes(tokens[token].name).length > 0, "Token does not exist");
+        // calculate price based on bonding curve
+
+        return 1;
+
+    }
 }
--- contracts/src/Token.sol
+++ contracts/src/Token.sol
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
 import "@openzeppelin-contracts-5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin-contracts-5.0.2/token/ERC20/extensions/ERC20Capped.sol";
 
-contract Token is ERC20, ERC20Burnable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract Token is ERC20, ERC20Capped, ERC20Burnable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint intialSupply,
+        uint maxSupply
+    ) ERC20(name, symbol) ERC20Capped(maxSupply) {
+        _mint(msg.sender, intialSupply);
+    }
 
     function decimals() public view virtual override returns (uint8) {
         return 1;
     }
 
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override(ERC20, ERC20Capped) {
+        ERC20Capped._update(from, to, value);
+    }
 }
